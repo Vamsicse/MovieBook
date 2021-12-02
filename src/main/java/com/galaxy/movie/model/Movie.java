a1fc27196d700d0fb3569fcbfd8b795f663ee4a3
azizijonespenn2 <61535698+azizijonespenn2@users.noreply.github.com>
2021-12-02 08:57:19 -0800

    Change to say what's up Doc.  Added getter/setters

--- src/main/java/com/galaxy/movie/model/Movie.java
+++ src/main/java/com/galaxy/movie/model/Movie.java
@@ -35,4 +35,39 @@ public class Movie {
         this.description = movie.description;
         this.releaseYear = movie.releaseYear;
     }
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+    
+    
 }
